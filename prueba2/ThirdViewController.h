f66c9271503b41797c7c8150ffe2e1fcde28e6a6
yeray <yeray@MacBook-Air-de-yeray.local>
2014-04-23 16:37:52 +0100

    Añadirle un NavigationController con un UViewController

---
+++ prueba2/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  prueba2
+//
+//  Created by yeray on 23/04/14.
+//  Copyright (c) 2014 www.yeraypv.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
